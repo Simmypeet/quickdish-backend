e543f930a1904d05b9774f549bb404640f7784c7
Simmypeet <simmypeet@outlook.com>
2024-09-16 23:17:14 +0700

    fix incorrect use of restaurant schema

--- api/schemas/restaurant.py
+++ api/schemas/restaurant.py
@@ -13,8 +13,20 @@ class RestaurantCreate(RestaurantBase):
     pass
 
 
-class Restaurant(RestaurantBase):
+class PublicRestaurant(RestaurantBase):
+    """
+    The schema for public restaurant data.
+    """
+
     id: int
     merchant_id: int
+
+
+class Restaurant(PublicRestaurant):
+    """
+    The schema for restaurant that includes all the information about the
+    restaurant. This schema should only be used internally.
+    """
+    image: str
 
     model_config = ConfigDict(from_attributes=True)
