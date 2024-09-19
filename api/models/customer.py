2ad132d4f59f2272ebcfc9620b09ad44cdcbd1ba
Simmypeet <simmypeet@outlook.com>
2024-09-19 17:09:57 +0700

    refactor models to use mapped_column instead

--- api/models/customer.py
+++ api/models/customer.py
@@ -1,15 +1,15 @@
 from api.models import Base
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 
-class Customer(Base):  # type:ignore
+class Customer(Base):
     __tablename__ = "customers"
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    username = Column(String, unique=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    salt = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str]
+    salt: Mapped[str]
--- api/models/restaurant.py
+++ api/models/restaurant.py
@@ -1,14 +1,17 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
 from api.models import Base
+from api.schemas.point import Point
 from api.types.point import PointType
 
 
 class Restaurant(Base):
     __tablename__ = "restaurants"
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String, unique=True, nullable=False)
-    address = Column(String, nullable=False)
-    merchant_id = Column(Integer, ForeignKey("merchants.id"))
-    image = Column(String, nullable=False)
-    location = Column(PointType, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    address: Mapped[str]
+    merchant_id: Mapped[int] = mapped_column(ForeignKey("merchants.id"))
+    image: Mapped[str]
+    location: Mapped[Point] = mapped_column(PointType)
