1c32cbae15f1d87360dc401839049574c9c0dba4
Simmypeet <simmypeet@outlook.com>
2024-11-14 16:45:23 +0700

    add event endpoint

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ import os
 import dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routers import customer, merchant, restaurant, order, admin
+from api.routers import customer, merchant, restaurant, order, admin, event
 
 
 app = FastAPI()
@@ -31,4 +31,5 @@ app.include_router(customer.router)
 app.include_router(merchant.router)
 app.include_router(restaurant.router)
 app.include_router(order.router)
+app.include_router(event.router)
 app.include_router(admin.router)
